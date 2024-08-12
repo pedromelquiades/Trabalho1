c4c095f9296783f22e133058c41b39006cd88dfe
pedromelquiades <pedromelquiadesdeoliveira@gmail.com>
2024-08-12 02:34:21 -0300

    Add files via upload

---
+++ Final.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome?')
+idade = int(input('Quantos anos você tem?'))
+valor1 = int('100')
+print('Olá',nome,'faltam',(valor1 - idade),'anos','para você completar 100 anos')
